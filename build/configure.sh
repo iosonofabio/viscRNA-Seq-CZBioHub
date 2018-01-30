df33313c4632dbf45630199172e5b72138a55de8
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-01-30 11:38:37 -0800

    Permissions of /assets

--- build/configure.sh
+++ build/configure.sh
@@ -17,35 +17,32 @@ hash -r
 echo "Configure conda for automation"
 conda config --set always_yes yes --set changeps1 no
 
-#echo "Update conda"
-#conda update -q conda
-#
-#echo "Install STAR"
-#conda install -y -c bioconda star 
-#
-#echo "Install HTSeq"
-#conda install -y -c bioconda htseq
-
-echo "Install cellranger"
-aws s3 cp s3://viscrna-seq/assets/software/cellranger/cellranger-2.1.0.tar.gz /tmp/cellranger.tar.gz
-tar -xvf /tmp/cellranger.tar.gz -C /tmp
-sudo mv /tmp/cellranger-2.1.0/* /usr/local/bin/
-cellranger sitecheck
+echo "Update conda"
+conda update -q conda
 
 echo "Compute combined transcriptome hashes"
 vir="NC_004065_1.gb"
 tra="refdata-cellranger-mm10-1.2.0"
+sudo mkdir /assets
+sudo chmod -R a+rwX /assets
 mkdir -p /assets/references/virus_genome
 mv /tmp/$vir /assets/references/virus_genome/$vir
 mkdir -p /assets/references/transcriptome
 cd /assets/transcriptome
 wget http://cf.10xgenomics.com/supp/cell-exp/$tra.tar.gz
 tar -xvf $tra.tar.gz
-/tmp/append_virus_to_transcriptome --virus-gb /assets/references/virus_genome/$vir --genome-fasta /assets/references/transcriptome/$tra/fasta/genome.fa --transcriptome-gtf /assets/references/transcriptome/$tra/genes/genes.gtf --output /assets/references/transcriptome/combined
+rm -rf $tra.tar.gz
+sudo /tmp/append_virus_to_transcriptome --virus-gb $HOME/assets/references/virus_genome/$vir --genome-fasta $HOME/assets/references/transcriptome/$tra/fasta/genome.fa --transcriptome-gtf $HOME/assets/references/transcriptome/$tra/genes/genes.gtf --output /assets/references/transcriptome/combined
 rm -rf /assets/references/transcriptome/$tra
 
 echo "Set up pipeline script server side"
 sudo mv /tmp/pipeline /usr/local/bin/
+
+echo "Install cellranger"
+aws s3 cp s3://viscrna-seq/assets/software/cellranger/cellranger-2.1.0.tar.gz /tmp/cellranger.tar.gz
+tar -xvf /tmp/cellranger.tar.gz -C /tmp
+sudo mv /tmp/cellranger-2.1.0/* /usr/local/bin/
+cellranger sitecheck
 
 echo "DONE"
 exit 0
