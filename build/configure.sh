86f027a5b0be7175f6f7bb890d922238aed852f3
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-01-30 14:56:10 -0800

    s3 cp quiet

--- build/configure.sh
+++ build/configure.sh
@@ -16,7 +16,7 @@ echo "Update conda"
 conda update -q conda
 
 echo "Install cellranger"
-aws s3 cp s3://viscrna-seq/assets/software/cellranger/cellranger-2.1.0.tar.gz /tmp/cellranger.tar.gz
+aws s3 cp --no-progress s3://viscrna-seq/assets/software/cellranger/cellranger-2.1.0.tar.gz /tmp/cellranger.tar.gz
 tar -xvf /tmp/cellranger.tar.gz -C /tmp
 sudo mv /tmp/cellranger-2.1.0/* /usr/local/bin/
 cellranger sitecheck
