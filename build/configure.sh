a0918c2aebcd6f8918d2bdf59800833a31e62e2c
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-01-30 20:09:46 -0800

    Install biopython

--- build/configure.sh
+++ build/configure.sh
@@ -15,30 +15,22 @@ conda config --set always_yes yes --set changeps1 no
 echo "Update conda"
 conda update -q conda
 
+echo "Install biopython"
+conda install -c bioconda biopython
+
 echo "Compute combined transcriptome hashes"
 vir="NC_004065_1.gb"
 tra="refdata-cellranger-mm10-1.2.0"
 
 echo "Make folders"
-sudo mkdir /assets
-sudo chmod -R a+rwX /assets
 mkdir -p /assets/references/virus_genome
 mv /tmp/$vir /assets/references/virus_genome/$vir
-mkdir -p /assets/references/transcriptome
-cd /assets/references/transcriptome
-
-echo "wget reference transcriptome"
-wget -nv http://cf.10xgenomics.com/supp/cell-exp/$tra.tar.gz
-
-echo "Extract reference transcriptome"
-tar -xf $tra.tar.gz
-rm -rf $tra.tar.gz
 
 echo "Make combined hashes"
-/tmp/append_virus_to_transcriptome --virus-gb $HOME/assets/references/virus_genome/$vir --genome-fasta $HOME/assets/references/transcriptome/$tra/fasta/genome.fa --transcriptome-gtf $HOME/assets/references/transcriptome/$tra/genes/genes.gtf --output /assets/references/transcriptome/combined
+/tmp/append_virus_to_transcriptome --virus-gb $HOME/assets/references/virus_genome/$vir --genome-fasta $HOME/assets/references/mouse/transcriptome/$tra/fasta/genome.fa --transcriptome-gtf $HOME/assets/references/mouse/transcriptome/$tra/genes/genes.gtf --output /assets/references/combined/transcriptome
 
-echo "Remove original transcriptome"
-rm -rf /assets/references/transcriptome/$tra
+echo "Remove original transcriptomes"
+rm -rf /assets/references/{mouse,human}
 echo "Combined transcriptome hashed"
 
 echo "Set up pipeline script server side"
