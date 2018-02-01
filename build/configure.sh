77faf03ca4b25ca291a7a8b06c2c33b59eaaa681
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-02-01 09:26:12 -0800

    Base off 10X_mouse_mCMV AMI

--- build/configure.sh
+++ build/configure.sh
@@ -15,35 +15,10 @@ conda config --set always_yes yes --set changeps1 no
 echo "Update conda"
 conda update -q conda
 
-echo "Install biopython"
-conda install -c bioconda biopython
-
-echo "Compute combined transcriptome hashes"
-vir="NC_004065_1.gb"
-tra="refdata-cellranger-mm10-1.2.0"
-
-echo "Make folders"
-mkdir -p /assets/references/virus_genome
-mv /tmp/$vir /assets/references/virus_genome/$vir
-
-# NOTE: rehashing is very expensive, so we cache it on S3
-#echo "Make combined hashes"
-#/tmp/append_virus_to_transcriptome --virus-gb /assets/references/virus_genome/$vir --genome-fasta /assets/references/mouse/transcriptome/$tra/fasta/genome.fa --transcriptome-gtf /assets/references/mouse/transcriptome/$tra/genes/genes.gtf --output /assets/references/combined/transcriptome
-#
-echo "Remove original transcriptomes"
-rm -rf /assets/references/{mouse,human}
-
-echo "Copy combined transcriptome from S3"
-aws s3 cp s3://viscrna-seq/assets/references/reference_mouse_and_mCMV.tar.gz /assets/references/mouse_and_mCMV.tar.gz
-echo "Extracting combined transcriptome"
-tar -xz /assets/reference/mouse_and_mCMV.tar.gz -C /assets/reference/
-echo "Removing tar.gz"
-rm -rf /assets/reference/mouse_and_mCMV.tar.gz
-echo "Combined transcriptome hashed"
-
 echo "Set up pipeline script server side"
-sudo mkdir /data
+sudo mkdir -p /data
 sudo chmod a+rwX /data
+sudo rm -rf /usr/local/bin/pipeline
 sudo mv /tmp/pipeline /usr/local/bin/
 
 echo "DONE"
