ab121a5ff9ecea328a005ca5d1bd46f549c0e1be
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-01-31 18:58:28 -0800

    Source combined genome hash from S3, too expensive ATM

--- build/configure.sh
+++ build/configure.sh
@@ -26,11 +26,19 @@ echo "Make folders"
 mkdir -p /assets/references/virus_genome
 mv /tmp/$vir /assets/references/virus_genome/$vir
 
-echo "Make combined hashes"
-/tmp/append_virus_to_transcriptome --virus-gb /assets/references/virus_genome/$vir --genome-fasta /assets/references/mouse/transcriptome/$tra/fasta/genome.fa --transcriptome-gtf /assets/references/mouse/transcriptome/$tra/genes/genes.gtf --output /assets/references/combined/transcriptome
-
+# NOTE: rehashing is very expensive, so we cache it on S3
+#echo "Make combined hashes"
+#/tmp/append_virus_to_transcriptome --virus-gb /assets/references/virus_genome/$vir --genome-fasta /assets/references/mouse/transcriptome/$tra/fasta/genome.fa --transcriptome-gtf /assets/references/mouse/transcriptome/$tra/genes/genes.gtf --output /assets/references/combined/transcriptome
+#
 echo "Remove original transcriptomes"
 rm -rf /assets/references/{mouse,human}
+
+echo "Copy combined transcriptome from S3"
+aws s3 cp s3://viscrna-seq/assets/references/reference_mouse_and_mCMV.tar.gz /assets/references/mouse_and_mCMV.tar.gz
+echo "Extracting combined transcriptome"
+tar -xz /assets/reference/mouse_and_mCMV.tar.gz -C /assets/reference/
+echo "Removing tar.gz"
+rm -rf /assets/reference/mouse_and_mCMV.tar.gz
 echo "Combined transcriptome hashed"
 
 echo "Set up pipeline script server side"
